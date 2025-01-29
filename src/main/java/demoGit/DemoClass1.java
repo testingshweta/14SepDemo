eeb7a95951459cffd489f069c1fea33d1e4467a1
shweta <velocitytesting2024@gmail.com>
2025-01-29 10:32:31 +0530

    first commit

---
+++ src/main/java/demoGit/DemoClass1.java
@@ -0,0 +1,11 @@
+package demoGit;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello00000");
+		System.out.println("hello00000222");
+	}
+
+}
